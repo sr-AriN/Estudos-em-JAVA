36b7606de2beb5ef27ee33c8839611f5c8383fc3
Ariel Santos Negreiros <sr.arinegreiros@gmail.com>
2022-05-16 00:31:55 -0300

    50% do proximo módulo fica pra amanhâ

---
+++ Estudos/src/LCCFJ/ControleFluxo/Main.java
@@ -0,0 +1,17 @@
+package LCCFJ.ControleFluxo;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private static void ifFlecha() {
+        int mes = 9;
+        if (mes == 1) {
+            System.out.println("Janeiro");
+        } else if (mes == 2) {
+            System.out.println("Fevereiro");
+        }
+    }
+
+}
